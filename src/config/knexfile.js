3446e78210d8c77ddc85b3d9d1c1708ffbf523ac
quangtudng <quangtudng@gmail.com>
2021-02-27 21:54:49 +0700

    :hammer: Added password

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -6,6 +6,7 @@ module.exports = {
       host : process.env.DB_HOST,
       user : process.env.DB_USER,
       database : process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
       port: process.env.DB_PORT,
       charset: 'utf8'
     },
