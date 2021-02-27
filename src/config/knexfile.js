d1902a1a4b617f1494a05f2543106f5921423a71
quangtudng <quangtudng@gmail.com>
2021-02-27 21:58:16 +0700

    :hammer: Fixed path again

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '../../.env' });
+require('dotenv').config({ path: __dirname + '/../../.env' });
 module.exports = {
   development: {
     client: process.env.DB_TYPE,
@@ -6,7 +6,6 @@ module.exports = {
       host : process.env.DB_HOST,
       user : process.env.DB_USER,
       database : process.env.DB_NAME,
-      password: process.env.DB_PASSWORD,
       port: process.env.DB_PORT,
       charset: 'utf8'
     },
