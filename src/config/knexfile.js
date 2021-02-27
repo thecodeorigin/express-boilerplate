fe3a6d1ce1828900914ff69b9063df5653b459d5
quangtudng <quangtudng@gmail.com>
2021-02-27 21:36:12 +0700

    :hammer: Added db password to ENV

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -7,6 +7,7 @@ module.exports = {
       user : process.env.DB_USER,
       database : process.env.DB_NAME,
       port: process.env.DB_PORT,
+      password: process.env.DB_PASSWORD,
       charset: 'utf8'
     },
     migrations: {
