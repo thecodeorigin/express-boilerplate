4ba3ff4fb7c7ce3a6afd3d5362bc643c381842e4
quangtudng <quangtudng@gmail.com>
2021-03-05 17:24:04 +0700

    :hammer: Test

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+console.log('test');
 // Router configuration
 router.forEach(route => {
   app[route.method]('/v1/' + route.prefix + route.path, ...route.handlers);
