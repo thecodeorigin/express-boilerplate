cef1e90ca2f3e180b95b8ed68d709be7c8ebe2be
quangtudng <quangtudng@gmail.com>
2021-03-18 23:25:07 +0700

    :hammer: Fixed the messed with git

--- src/app/Auth/index.controller.js
+++ src/app/Auth/index.controller.js
@@ -1,20 +1,60 @@
 require('dotenv').config({ path: __dirname + '/../../../.env' });
 const jwt = require('jsonwebtoken');
 const userService = require('../User/index.service');
-const login = async (req, res, next) => {
-  const user = await userService.getOneByEmail(req.body.email);
+const bcrypt = require('bcrypt');
+const { HTTPException } = require('../../common/helpers/errorHandler');
 
-  const token = jwt.sign(JSON.parse(JSON.stringify(user)), process.env.JWT_SECRET);
-  return res.json({
-    token,
-    user,
-  });
+const login = async (req, res, next) => {
+  try {
+    const user = await userService.getOneByEmail(req.body.email);
+    if(!user) {
+      return res.json({
+        
+      })
+    }
+    const token = jwt.sign(
+      JSON.parse(JSON.stringify(user)),
+      process.env.JWT_SECRET, 
+      { expiresIn: process.env.JWT_EXPIRE, noTimestamp: true }
+    );
+    return res.json({
+      user,
+      token,
+    });
+  } catch (err) {
+    next(err)
+  }
 };
 
 const register = async (req, res, next) => {
-  
+  try {
+    const user = await userService.createOne(req.body.email, req.body.name);
+    
+  } catch (err) {
+    
+  }
+};
+
+const getMe = async (req, res, next) => {
+  try {
+    const token = req.headers.token;
+    if(!token) {
+      throw new HTTPException(400, "JWT token is empty")
+    }
+    return res.json(jwt.verify(token, process.env.JWT_SECRET)); 
+  } catch (error) {
+    console.log(error);
+    if(error instanceof jwt.TokenExpiredError) {
+      next(new HTTPException(400, "JWT token is expired"));
+    }
+    if(error instanceof jwt.JsonWebTokenError) {
+      next(new HTTPException(400, "JWT token format is incorrect"));
+    }
+    next(error);
+  }
 };
 module.exports = {
   login,
   register,
+  getMe,
 };
--- src/app/Auth/index.router.js
+++ src/app/Auth/index.router.js
@@ -1,6 +1,7 @@
-const {register, login} = require('./index.controller');
+const {register, login, getMe} = require('./index.controller');
 
 module.exports = [
   { prefix: 'auth', method: 'post', path: `/register`, handlers: [register] },
-  { prefix: 'auth', method: 'patch', path: `/login`, handlers: [login] },
+  { prefix: 'auth', method: 'post', path: `/login`, handlers: [login] },
+  { prefix: 'auth', method: 'get', path: '/me', handlers: [getMe] }
 ];
