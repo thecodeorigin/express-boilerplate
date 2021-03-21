e4c899c71f00c467371c040bc2be583b4e19dc53
quangtudng <quangtudng@gmail.com>
2021-03-21 20:01:40 +0700

    :hammer: Changed payload for services

--- src/app/User/index.service.js
+++ src/app/User/index.service.js
@@ -8,15 +8,12 @@ const getOneById = (id) => {
   return knex('users').where({ id }).first('*');
 };
 
-const createOne = async (email, name, password) => {
-  return knex('users').insert({ email, name, password });
+const createOne = async (payload) => {
+  return knex('users').insert(payload);
 };
 
-const patchOne = async (id, email, name) => {
-  return knex('users').where({ id }).update({
-    email: email ? email : user.email,
-    name: name ? name : user.name 
-  });
+const patchOne = async (id, payload) => {
+  return knex('users').where({ id }).update(payload);
 };
 
 const deleteOne = async (id) => {
