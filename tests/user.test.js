89a8f42d353e4d4e53b6ed4f2433f74ce6d59c37
quangtudng <quangtudng@gmail.com>
2021-03-22 15:57:10 +0700

    :hammer: Updated some tests and changed production script

--- tests/user.test.js
+++ tests/user.test.js
@@ -8,7 +8,28 @@ describe("/users endpoint", () => {
     expect(response.statusCode).toBe(400);
     done();
   });
-  afterAll(() => {
+  test('GET /users/:id should return 400 if user is not logged in', async (done) => {
+    const id = 1;
+    const response = await request(server).get(`/v1/users/${id}`);
+    expect(response.statusCode).toBe(400);
+    done();
+  });
+  test('GET /users should return 400 if user is not logged in', async (done) => {
+    const response = await request(server).get('/v1/users');
+    expect(response.statusCode).toBe(400);
+    done();
+  });
+  test('GET /users should return 400 if user is not logged in', async (done) => {
+    const response = await request(server).get('/v1/users');
+    expect(response.statusCode).toBe(400);
+    done();
+  });
+  test('GET /users should return 400 if user is not logged in', async (done) => {
+    const response = await request(server).get('/v1/users');
+    expect(response.statusCode).toBe(400);
+    done();
+  });
+  afterEach(() => {
     knex.destroy();
     server.close();
   });
