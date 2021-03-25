e173231be713b7d339cffbce8bed222277faacf9
quangtudng <quangtudng@gmail.com>
2021-03-25 10:41:45 +0700

    :hammer: Test circle

--- src/constants/httpMessage.js
+++ src/constants/httpMessage.js
@@ -2,7 +2,7 @@ module.exports = {
   USER_NOT_FOUND: 'User not found',
   EMAIL_EXISTS: 'User with that email already exists',
   INVALID_CREDENTIAL: 'Email or password is incorrect',
-  LOGIN_REQUIRED: 'You must be login to use this feature',
+  LOGIN_REQUIRED: 'You must be login to use this protected feature',
   UNEXPECTED_EXCEPTION:
     'An unexpected error has occurred. Please try again or contact the admin',
 };
